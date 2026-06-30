4e8b38a16b3779fe76821efaca16e71e9e53e3b2
Lukasz Sliczner <lukasz.sliczner@interia.pl>
2026-06-30 18:16:24 +0000

    Add the tests for IP validatiion

---
+++ test_ip_validatro_class.py
@@ -0,0 +1,24 @@
+import unittest
+from ip_validator import IPValidator
+
+class TestIPValidator(unittest.TestCase):
+    def setUp(self):
+        self.validator = IPValidator()
+
+    def test_valid_ipv4(self):
+        self.assertTrue(self.validator.is_valid_ip("192.168.1.1"))
+
+    def test_invalid_ipv4_letters(self):
+        self.assertFalse(self.validator.is_valid_ip("192.abc.1.1")) 
+
+    def test_invalid_ipv4_out_of_range(self):
+        self.assertFalse(self.validator.is_valid_ip("256.100.50.25"))
+
+    def test_invalid_ipv4_too_few_octets(self):
+        self.assertFalse(self.validator.is_valid_ip("192.168.1"))
+
+    def test_invalid_ipv4_too_many_octets(self):
+        self.assertFalse(self.validator.is_valid_ip("10.10.10.10.10"))  
+
+if __name__ == '__main__':
+    unittest.main(verbose=2)   
