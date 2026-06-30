c56bebb1cfa9965ad304af04fc3503aacf523279
Lukasz Sliczner <lukasz.sliczner@interia.pl>
2026-06-30 18:35:07 +0000

    Add the logic to the IPValidator class.

---
+++ ip_validator.py
@@ -0,0 +1,30 @@
+class IPValidator:
+    # def __init__(self, ip_address):
+    #     self.ip_address = ip_address
+
+    def is_valid_ipv4(self, ip_address):
+        # pass  # Placeholder for IPv4 validation logic
+        parts = ip_address.split('.')
+        if len(parts) != 4:
+            return False
+        for part in parts:
+            if not part.isdigit() or not 0 <= int(part) <= 255:
+                return False
+        return True
+
+    # def is_valid_ipv6(self):
+    #     parts = self.ip_address.split(':')
+    #     if len(parts) != 8:
+    #         return False
+    #     for part in parts:
+    #         if len(part) > 4 or not all(c in '0123456789abcdefABCDEF' for c in part):
+    #             return False
+    #     return True
+
+    # def validate(self):
+    #     if self.is_valid_ipv4():
+    #         return "Valid IPv4"
+    #     elif self.is_valid_ipv6():
+    #         return "Valid IPv6"
+    #     else:
+    #         return "Invalid IP address"
--- test_ip_validatro_class.py
+++ test_ip_validatro_class.py
@@ -6,19 +6,19 @@ class TestIPValidator(unittest.TestCase):
         self.validator = IPValidator()
 
     def test_valid_ipv4(self):
-        self.assertTrue(self.validator.is_valid_ip("192.168.1.1"))
+        self.assertTrue(self.validator.is_valid_ipv4("192.168.1.1"))
 
     def test_invalid_ipv4_letters(self):
-        self.assertFalse(self.validator.is_valid_ip("192.abc.1.1")) 
+        self.assertFalse(self.validator.is_valid_ipv4("192.abc.1.1")) 
 
     def test_invalid_ipv4_out_of_range(self):
-        self.assertFalse(self.validator.is_valid_ip("256.100.50.25"))
+        self.assertFalse(self.validator.is_valid_ipv4("256.100.50.25"))
 
     def test_invalid_ipv4_too_few_octets(self):
-        self.assertFalse(self.validator.is_valid_ip("192.168.1"))
+        self.assertFalse(self.validator.is_valid_ipv4("192.168.1"))
 
     def test_invalid_ipv4_too_many_octets(self):
-        self.assertFalse(self.validator.is_valid_ip("10.10.10.10.10"))  
+        self.assertFalse(self.validator.is_valid_ipv4("10.10.10.10.10"))  
 
 if __name__ == '__main__':
-    unittest.main(verbose=2)   
+    unittest.main(verbosity=2)   
